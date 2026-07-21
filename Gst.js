6acaac184389bbf71b1b3d8e3c05b035f375a3ef
Ankit! <rajakankit7815@gmail.com>
2026-07-21 20:51:50 +0530

    Add files via upload

--- Gst.js
+++ Gst.js
@@ -16,3 +16,46 @@ function calculateGST(){
         "GST (" + gst + "%): ₹" + gstAmount.toFixed(2) + "<br>" +
         "<span style='color:green'>Total: ₹" + total.toFixed(2) + "</span>";
 }
+function addGST(){
+
+let amount=parseFloat(document.getElementById("amount").value);
+let gst=parseFloat(document.getElementById("gst").value);
+
+if(isNaN(amount)||isNaN(gst)){
+alert("Enter Amount & GST%");
+return;
+}
+
+let gstAmount=amount*gst/100;
+let total=amount+gstAmount;
+
+document.getElementById("result").innerHTML=
+`
+<b>Original Amount:</b> ₹${amount.toFixed(2)}<br>
+<b>GST (${gst}%):</b> ₹${gstAmount.toFixed(2)}<br>
+<b style="color:green;">Total:</b> ₹${total.toFixed(2)}
+`;
+
+}
+
+function removeGST(){
+
+let total=parseFloat(document.getElementById("amount").value);
+let gst=parseFloat(document.getElementById("gst").value);
+
+if(isNaN(total)||isNaN(gst)){
+alert("Enter Amount & GST%");
+return;
+}
+
+let original=total/(1+gst/100);
+let gstAmount=total-original;
+
+document.getElementById("result").innerHTML=
+`
+<b>Total Amount:</b> ₹${total.toFixed(2)}<br>
+<b>Original Amount:</b> ₹${original.toFixed(2)}<br>
+<b>GST (${gst}%):</b> ₹${gstAmount.toFixed(2)}
+`;
+
+}
