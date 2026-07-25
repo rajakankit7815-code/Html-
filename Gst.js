43d2235a0466d22f6ef0c73d1d949bea7a70d545
Ankit! <rajakankit7815@gmail.com>
2026-07-25 19:55:45 +0530

    Add files via upload

--- Gst.js
+++ Gst.js
@@ -95,3 +95,45 @@ IGST (${gst}%) : ₹${igst.toFixed(2)}<br><br>
 `;
 
 }
+const display=document.getElementById("display");
+
+function append(value){
+    display.value+=value;
+}
+
+function clearDisplay(){
+    display.value="";
+}
+
+function deleteLast(){
+    display.value=display.value.slice(0,-1);
+}
+
+function calculate(){
+    try{
+        display.value=eval(display.value);
+    }catch{
+        display.value="Error";
+    }
+}
+
+document.addEventListener("keydown",(e)=>{
+
+if((e.key>='0' && e.key<='9') ||
+['+','-','*','/','.','%'].includes(e.key)){
+display.value+=e.key;
+}
+
+if(e.key==="Enter"){
+calculate();
+}
+
+if(e.key==="Backspace"){
+deleteLast();
+}
+
+if(e.key==="Escape"){
+clearDisplay();
+}
+
+});
